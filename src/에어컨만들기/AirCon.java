317ea7acdfd556d9ef0c3c1f32f77267086ad93f
jks2024 <jks2024@gmail.com>
2026-03-16 11:48:12 +0900

    에어컨

--- src/에어컨만들기/AirCon.java
+++ src/에어컨만들기/AirCon.java
@@ -11,6 +11,10 @@ package 에어컨만들기;
 // 각각의 필드에 대한 게터와 세터 만들기
 // 예어컨의 전체 정보 상태를 보여는 메서드 만들기
 
+import java.util.Calendar;
+
+import static java.lang.Thread.sleep;
+
 public class AirCon {
     boolean power;  // 전원 : ON/OFF
     int tempSet;    // 온도 설정
@@ -20,71 +24,72 @@ public class AirCon {
     int wind;       // 1/2/3
 
     public AirCon() {
+        final int[] monthTempArr = {-5, 3, 10, 15, 22, 28, 32, 30, 24, 16, 8, 4};
+        Calendar cal = Calendar.getInstance();
+        int month = cal.get(Calendar.MONTH); // 현재의 몇월인지 정보를 가져 옴.
+        tempNow = monthTempArr[month];  // 현재달의 온도를 현재 온도로 대입 받음
         power = false;
         tempSet = 20;  // 설정 온도
-        tempNow = 20;  // 현재 온도
         cooler = false;
         heater = false;
         wind = 1;
-    }
-
-    public boolean isPower() {
-        return power;
-    }
-
-    public void setPower(boolean power) {
-        this.power = power;
-    }
-
-    public int getTempSet() {
-        return tempSet;
-    }
-
-    public void setTempSet(int tempSet) {
-        this.tempSet = tempSet;
-    }
-
-    public int getTempNow() {
-        return tempNow;
-    }
-
-    public void setTempNow(int tempNow) {
-        this.tempNow = tempNow;
-    }
-
-    public boolean isCooler() {
-        return cooler;
-    }
-
-    public void setCooler(boolean cooler) {
-        this.cooler = cooler;
-    }
-
-    public boolean isHeater() {
-        return heater;
-    }
-
-    public void setHeater(boolean heater) {
-        this.heater = heater;
-    }
-
-    public int getWind() {
-        return wind;
-    }
-
-    public void setWind(int wind) {
-        this.wind = wind;
     }
 
     public void printAirCon() {
         final String[] step = {"", "1단계", "2단계", "3단계"};
         System.out.println("======= 에어컨 정보 =======");
         System.out.println("전원 " + (power ? "ON" : "OFF") );
-        System.out.println("현재 온도 : " + getTempNow());
-        System.out.println("설정 온도 : " + getTempSet());
+        System.out.println("현재 온도 : " + tempNow);
+        System.out.println("설정 온도 : " + tempSet);
         System.out.println("냉방 모드 : " + (cooler ? "ON" : "OFF"));
         System.out.println("난방 모드 : " + (heater ? "ON" : "OFF"));
         System.out.println("바람 세기 : " + step[wind]);
+    }
+
+    // 에어컨 동작 메서드
+    public void operate() {
+        int elapsedTime = 0;  // 경과 시간을 계산하기 위한 변수
+        boolean changedTemp = false;  // 온도를 변경해야 하는 조건
+
+        System.out.println("\n===== 에어컨 작동을 시작 합니다. =====");
+        printAirCon();
+
+        try {
+            while (true) {
+                sleep(1000);  // 1초 잠재움
+                elapsedTime++;
+                int getTimeThreshold = getTimeFunc();  // 바람세기에 따른 시간값 가져 옴
+
+                if (elapsedTime >= getTimeThreshold) {  // 온도를 바꿔야 할 상황
+                    changedTemp = true;
+                }
+
+                if (changedTemp) {  // 온도를 변경해야 할 조건
+                    if (cooler) setTempNow(-1);
+                    if (heater) setTempNow(1);
+                    printAirCon();
+                    elapsedTime = 0;  // 경과 시간을 초기화
+                    changedTemp = false; // 온도 변경 조건을 false로 변경
+                }
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    // 현재 온도 변경 메서드
+    private void setTempNow(int temp) {
+        tempNow += temp;
+    }
+
+    private int getTimeFunc() {
+        switch (wind) {
+            case 1: return  60;
+            case 2: return  30;
+            case 3: return  20;
+            default: return 60;
+        }
     }
 
 }
