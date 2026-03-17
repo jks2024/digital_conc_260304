0d4e04831014bfe656d8e5b64a4012284c618f9d
jks2024 <jks2024@gmail.com>
2026-03-17 11:41:51 +0900

    자동차만들기

---
+++ src/자동차만들기/Car.java
@@ -0,0 +1,64 @@
+package 자동차만들기;
+
+import java.text.DecimalFormat;
+
+import static 자동차만들기.Common.FUEL_PRICE;
+
+public abstract class Car {
+    protected int maxSpeed; // 최고 속도
+    protected int fuelEff; // 연비 (리터당 주행 거리(Km))
+    protected int fuelTank; // 연료 탱크 크기
+    protected int seatCnt; // 좌석수
+    protected String name; // 자동차 이름
+
+    // 매개변수가 있는 생성자, 기본생성자는 없음
+    public Car(int maxSpeed, int fuelEff, int fuelTank, int seatCnt, String name) {
+        this.maxSpeed = maxSpeed;
+        this.fuelEff = fuelEff;
+        this.fuelTank = fuelTank;
+        this.seatCnt = seatCnt;
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    // 총 이동 횟수
+    int getMovingCnt(int passCnt) {
+        return (int) Math.ceil((double) passCnt / seatCnt);
+    }
+
+    // 총 주유 횟수
+    int getRefuelCnt(int dist, int moveCnt) { // 이동 거리와 이동 횟수를 매개 변수로 전달 받음
+        double totalDistance = dist * moveCnt;
+        double totalFuel = totalDistance / fuelEff;  // 거리 / 연비
+        return (int) Math.ceil(totalFuel / fuelTank); // 연료소모량 / 연료통 크기
+    }
+
+    // 총 이동 비용
+    String getTotalCost(int dist, int moveCnt) {
+        double totalDistance = dist * moveCnt;
+        double totalFuel = totalDistance / fuelEff;
+        long totalCost = Math.round(totalFuel * FUEL_PRICE);
+        DecimalFormat df = new DecimalFormat("#,###");
+        return df.format(totalCost);
+    }
+
+    // 총 소요 시간
+    String getMovingTime(int dist, int moveCnt, int weather) {
+        double getTime = (double) dist * moveCnt / maxSpeed;
+
+        switch (weather) {
+            case 2: getTime *= 1.2; break;
+            case 3: getTime *= 1.4; break;
+            default:
+        }
+
+        int totalMinute = (int) (getTime * 60);
+        int hour = totalMinute / 60;
+        int minute = totalMinute % 60;
+        return String.format("%d시간 %d분", hour, minute);
+    }
+
+    abstract void setMode(boolean isMode); // 상속 받은 각각의 자동차의 특수 기능
+
+}
---
+++ src/자동차만들기/CarMain.java
@@ -0,0 +1,66 @@
+package 자동차만들기;
+
+import java.util.Scanner;
+
+import static 자동차만들기.Common.DISTANCE;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Car car = null;  // 부모 클래스의 참조 변수
+        int loc = 0, cnt = 0, type = 0, option = 0, weather = 0;
+
+        // 이동 지역
+        while (true) {
+            System.out.print("\n이동 지역 [1]부산 [2]대전 [3]강릉 [4]광주 : ");
+            loc = sc.nextInt();
+            if (loc >= 1 && loc <= 4) break;
+            System.out.println("이동할 지역 선택이 잘못되었습니다.");
+        }
+        // 승객 수
+        while (true) {
+            System.out.print("이동할 승객 수 입력 (1~100): ");
+            cnt = sc.nextInt();
+            if (cnt >= 1 && cnt <= 100) break;
+            System.out.println("승객 수는 1~100명 사이여야 합니다.");
+        }
+        // 차량 선택
+        while (true) {
+            System.out.print("차량 선택 [1]스포츠카 [2]승용차 [3]버스 : ");
+            type = sc.nextInt();
+            if (type >= 1 && type <= 3) break;
+            System.out.println("차량 선택이 잘못되었습니다.");
+        }
+        // 부가 기능
+        while (true) {
+            System.out.print("부가 기능 [1]ON [2]OFF : ");
+            option = sc.nextInt();
+            if (option == 1 || option == 2) break;
+            System.out.println("모드 선택이 잘못되었습니다.");
+        }
+        boolean isMode = (option == 1);
+
+        // 자동차 객체 대입
+        switch (type) {
+            case 1: car = new SportCar("람로르기니"); break;
+            case 2: car = new SportCar("G80"); break;
+            case 3: car = new Bus("리무진 버스"); break;
+            default: System.out.println("없는 차량 입니다.");
+        }
+
+        car.setMode(isMode);
+
+        while (true) {
+            System.out.print("날씨 선택 [1]맑음 [2]비 [3]눈 : ");
+            weather = sc.nextInt();
+            if (weather >= 1 && weather <= 3) break;
+            System.out.println("날씨 선택이 잘못되었습니다.");
+        }
+        int moveCnt = car.getMovingCnt(cnt);
+        System.out.println("=".repeat(7) + car.getName() + "=".repeat(7));
+        System.out.println("총 비용 : " + car.getTotalCost(DISTANCE[loc], moveCnt) + "원");
+        System.out.println("총 주유 횟수 : " + car.getRefuelCnt(DISTANCE[loc], moveCnt) + "회");
+        System.out.println("총 이동 시간 : " + car.getMovingTime(DISTANCE[loc], moveCnt, weather));
+
+    }
+}
