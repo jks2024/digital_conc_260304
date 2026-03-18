1b48793e7a1e43270291c7bcb7ccca099904284e
jks2024 <jks2024@gmail.com>
2026-03-18 10:40:12 +0900

    자동차만들기3

--- src/자동차만들기/Bus.java
+++ src/자동차만들기/Bus.java
@@ -1,8 +1,13 @@
 package 자동차만들기;
 
-public class Bus extends Car {
+public class Bus extends Car implements AutoDrive, AirCon {
+    private boolean isAutoDrive;
+    private boolean isAirCon;
+
     public Bus(String name) {
         super(150, 5, 100, 20, name);
+        isAutoDrive = false;
+        isAirCon = false;
     }
 
     @Override
@@ -12,11 +17,23 @@ public class Bus extends Car {
 
     @Override
     void applyOptions() {
-
+        if (isAirCon) fuelEff = (int) (fuelEff * 0.95);
+        if (isAutoDrive) maxSpeed = (int) (maxSpeed * 0.90);
     }
 
     @Override
     void printOptions() {
+        System.out.println("에어컨 : " + (isAirCon ? "ON" : "OFF") );
+        System.out.println("자율주행 : " + (isAutoDrive ? "ON" : "OFF"));
+    }
 
+    @Override
+    public void airConOnOff(boolean isOn) {
+        this.isAirCon = isOn;
+    }
+
+    @Override
+    public void autoOnOff(boolean isOn) {
+        this.isAutoDrive = isOn;
     }
 }
