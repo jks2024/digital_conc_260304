832df47e18bcd49873ddf8b050415188b0307a89
jks2024 <jks2024@gmail.com>
2026-03-24 09:40:04 +0900

    정렬

---
+++ src/컴페어러블/CompMain.java
@@ -0,0 +1,10 @@
+package 컴페어러블;
+// Comparable과 Comparator는 자바에서 객체를 정렬하는 데 사용하는 인터페이스
+// Comparable : 나와 다른 객체를 비교하는 방식
+// Comparator : 다른 두 객체를 비교하는 방식
+
+public class CompMain {
+    public static void main(String[] args) {
+
+    }
+}
